5c9b8130aecfe5425498ba829c94bf3f8d1cdd79
Nasser <nasserbenyed@gmail.com>
2021-04-14 00:27:33 -0700

    Updates

---
+++ precache-manifest.cfb200dfb5f1764bb46d0666f5f204cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df0b746f56c8350f47db29145a3d8f5",
+    "url": "/ImgApi/index.html"
+  },
+  {
+    "revision": "1ae0d2a81c0ddcb7e5ec",
+    "url": "/ImgApi/static/css/main.daba7ea2.chunk.css"
+  },
+  {
+    "revision": "94c37d2e81bb9b53d3b4",
+    "url": "/ImgApi/static/js/2.6387efd6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ImgApi/static/js/2.6387efd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae0d2a81c0ddcb7e5ec",
+    "url": "/ImgApi/static/js/main.8eccfa60.chunk.js"
+  },
+  {
+    "revision": "8425f34aa74ee3a11304",
+    "url": "/ImgApi/static/js/runtime-main.42741c63.js"
+  }
+]);
